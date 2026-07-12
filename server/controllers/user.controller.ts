cbe164823bf4d9ad2e718d3e238f425dc7ffd131
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-07-12 23:18:26 +0500

    Implement user registration and email activation functionality

---
+++ server/controllers/user.controller.ts
@@ -0,0 +1,79 @@
+require('dotenv').config();
+import { Request,Response,NextFunction } from "express";
+import userModel from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { catchAsyncError } from "../middleware/catchAsyncError";
+import jwt, { Secret } from 'jsonwebtoken'
+import ejs from "ejs"
+import path from "node:path";
+import sendMail from "../utils/sendMail";
+
+
+//register User
+interface IRegisterationBody{
+    name:string,
+    email:string,
+    password:string,
+    avatar?:string,
+}
+
+export const registerationUser = catchAsyncError(async (req:Request,res:Response,next:NextFunction) => {
+    try {
+        const {name,email,password} = req.body;
+
+        const isEmailExist = await userModel.findOne({email});
+        if(isEmailExist) {
+            return next(new ErrorHandler("Email already exist",400))
+        }
+
+        const user:IRegisterationBody = {
+            name,
+            email,
+            password
+        }
+
+        const activationToken = createActivationToken(user);
+
+        const activationCode = activationToken.activationCode;
+
+        const data = {user:{name:user.name},activationCode};
+        const html = await ejs.renderFile(path.join(__dirname,"../mails/activation-mail.ejs"));
+
+       try {
+            await sendMail({
+                email: user.email,
+                subject: "Activate your Account",
+                template: "activation-mail.ejs",
+                data,
+            })
+
+            res.status(201).json({
+                success: true,
+                message: `Please check your email:${user.email} to activate your account`,
+                activationToken: activationToken.token
+            })
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message, 400))
+        }
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+    }
+})
+
+
+interface IActivationToken {
+    token:string,
+    activationCode:string
+}
+
+
+export const createActivationToken = (user: any): IActivationToken => {
+    const activationCode = Math.floor(1000 + Math.random() * 9000).toString(); //this will generate a 4 digit number
+    const token = jwt.sign(
+        { user, activationCode },
+        process.env.ACTIVATION_SECRET as Secret,
+        { expiresIn: "5m" }
+    )
+    return { token, activationCode }
+}
