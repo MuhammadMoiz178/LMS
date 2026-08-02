415f56f8874f97a7d01b29e781ebacba8b021497
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-08-02 14:30:10 +0500

    Add get single course functionality to course routes

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -61,3 +61,18 @@ export const editCourse = catchAsyncError(async (req:Request,res:Response,next:N
         return next(new ErrorHandler(error.message,500));
     }
 })
+
+
+//get single course - without purchasing
+export const getSingleCourse = catchAsyncError(async (req:Request,res:Response,next:NextFunction) => {
+    try {
+        const course = await courseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        res.status(200).json({
+            success:true,
+            course
+        })
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message,500));
+    }
+})
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,9 +1,12 @@
 import express from 'express'
-import { editCourse, uploadCourse } from '../controllers/course.controller';
+import { editCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizeRoles, isAuthenicated } from '../middleware/auth';
 const courseRouter = express.Router();
 
 courseRouter.post('/create-course',isAuthenicated,authorizeRoles("admin"),uploadCourse);
+
 courseRouter.put('/edit-course/:id',isAuthenicated,authorizeRoles("admin"),editCourse);
+
+courseRouter.get('/get-course/:id',getSingleCourse);
 
 export default courseRouter
