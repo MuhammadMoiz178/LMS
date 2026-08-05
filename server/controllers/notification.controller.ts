54b0ebbb95524e0f49b6f7975a489529494cf5f0
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-08-05 18:25:29 +0500

    Add update notification functionality for admin users

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -13,6 +13,30 @@ export const getNotifications = catchAsyncError(async (req:Request,res:Response,
             notifications
         })
     } catch (error:any) {
-        return next(new ErrorHandler(error.message,500))
+        return next(new ErrorHandler(error.message,500));
+    }
+})
+
+// update notification status - only admin
+export const updateNotification = catchAsyncError(async (req:Request,res:Response,next:NextFunction) => {
+    try {
+        const notification = await notificationModel.findById(req.params.id);
+        
+        if(!notification) {
+            return next(new ErrorHandler("Notoification not found",404))
+        } else {
+            notification.status ?  notification.status = "read" : notification.status; 
+        }
+
+        await notification.save();
+
+        const notifications = await notificationModel.find().sort({createdAt:-1})
+
+        res.status(201).json({
+            success:true,
+            notifications
+        })
+    } catch (error:any) {
+       return next(new ErrorHandler(error.message,500));
     }
 })
--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { authorizeRoles, isAuthenicated } from '../middleware/auth';
-import { getNotifications } from '../controllers/notification.controller';
+import { getNotifications, updateNotification } from '../controllers/notification.controller';
 
 const notificationRouter = express.Router();
 
 notificationRouter.get('/get-all-notifications',isAuthenicated,authorizeRoles("admin"),getNotifications);
+
+notificationRouter.put('/update-notifications/:id',isAuthenicated,authorizeRoles("admin"),updateNotification);
 
 
 export default notificationRouter
